19e20696c96626f3958c502a62ddfd6fe9401241
Kaldirion <kaldirion@gmail.com>
2014-12-21 21:27:35 +0200

    AmberStore

--- CodeSchool/AmberStore/app/controller/StoreController.js
+++ CodeSchool/AmberStore/app/controller/StoreController.js
@@ -1,12 +1,21 @@
 app.controller('StoreController',function($scope){
 
-    var gem = {
-        name : 'Dodecahedron',
-        price : 2.95,
-        description : 'Some gems have hidden qualities beyond their luster, beyond their shine ... Dobeca is one of those gems.',
-        canPurchase : false
-    }
+    var gems = [{
+            name : 'Dodecahedron',
+            price : 2.95,
+            description : 'Some gems have hidden qualities beyond their luster, beyond their shine ... Dobeca is one of those gems.',
+            canPurchase : true,
+            soldOut: false
+        },
+        {
+            name : 'Pentagonal Gem',
+            price : 5.95,
+            description : 'Some gems have hidden qualities beyond their luster, beyond their shine ... Dobeca is one of those gems.',
+            canPurchase : false,
+            soldOut: false
+        }
+    ];
 
-    $scope.storeProduct = gem;
+    $scope.storeProducts = gems;
 
 })
