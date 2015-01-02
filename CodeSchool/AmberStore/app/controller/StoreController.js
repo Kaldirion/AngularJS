c671c88ed31dc71c354f8d1b6bb43ef3de318e48
Kaldirion <kaldirion@gmail.com>
2015-01-02 21:22:12 +0200

    Amber Store

--- CodeSchool/AmberStore/app/controller/StoreController.js
+++ CodeSchool/AmberStore/app/controller/StoreController.js
@@ -1,9 +1,27 @@
 app.controller('StoreController',function($scope){
 
     var gems = [{
-            name : 'Dodecahedron',
-            price : 2.95,
-            description : 'Some gems have hidden qualities beyond their luster, beyond their shine ... Dobeca is one of those gems.',
+            name : 'Amethyst',
+            price : 199,
+            description : 'Amethyst is the purple variety of the quartz mineral species and the gem most commonly associated with the color purple',
+            images : [
+                {
+                    full : 'app/assets/img/Amethyst/full/Amethyst_ovale_01.jpg',
+                    thumb : 'app/assets/img/Amethyst/thumb/Amethyst_ovale_01.jpg'
+                },
+                {
+                    full : 'app/assets/img/Amethyst/full/Amethyst_ovale_02.jpg',
+                    thumb: 'app/assets/img/Amethyst/thumb/Amethyst_ovale_02.jpg'
+                },
+                {
+                    full : 'app/assets/img/Amethyst/full/Amethyst_square_01.jpg',
+                    thumb : 'app/assets/img/Amethyst/thumb/Amethyst_square_01.jpg'
+                },
+                {
+                    full : 'app/assets/img/Amethyst/full/Amethyst_triangle_01.jpg',
+                    thumb : 'app/assets/img/Amethyst/thumb/Amethyst_triangle_01.jpg'
+                }
+            ],
             canPurchase : true,
             soldOut: false
         },
