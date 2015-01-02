8dea045b9afd4ac305e3feb5de5938b7cfac4a4e
Kaldirion <kaldirion@gmail.com>
2015-01-02 22:29:57 +0200

    basic structure of used templates

--- Ads Project/app/js/app.js
+++ Ads Project/app/js/app.js
@@ -1,3 +1,27 @@
-/**
- * Created by Daniel on 2.1.2015 г..
- */
+var adsApp = angular.module('adsApp',['ngRoute']);
+
+adsApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/templates/adsHome.html'
+            })
+            .when('/register', {
+                templateUrl: 'app/templates/registerForm.html'
+            })
+            .when('/login',{
+                templateUrl: 'app/templates/loginForm.html'
+            })
+            .when('/user/home',{
+                templateUrl: 'app/templates/user/userHome.html'
+            })
+            .when('/user/ads/publish',{
+                templateUrl: 'app/templates/user/publishAd.html'
+            })
+            .when('/user/ads', {
+                templateUrl: 'app/templates/user/userAds.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
